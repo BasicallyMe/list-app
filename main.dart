e3dedf9ec440b1fb94439c3193ea448d1a4a7527
Abajit Dey <78209391+BasicallyMe@users.noreply.github.com>
2021-07-14 12:09:14 +0530

    Safe upload
    Different structure for the list app. Need to work on delete and edit functionality

--- main.dart
+++ main.dart
@@ -14,147 +14,77 @@ class ShoppingCart extends StatelessWidget {
   }
 }
 
-class ShoppingList extends StatelessWidget {
-  ShoppingList({required this.text, required this.animationController});
-  final String text;
-  final AnimationController animationController;
-  @override
-  Widget build(BuildContext context) {
-    return SizeTransition(
-      sizeFactor:
-          CurvedAnimation(parent: animationController, curve: Curves.bounceOut),
-      axisAlignment: 0.0,
-      child: Card(
-        child: ListTile(
-          title: Text(
-            text,
-            style: TextStyle(color: Colors.white, fontSize: 20.0),
-          ),
-        ),
-        color: Color.fromRGBO(255, 255, 255, 0.3),
-        elevation: 0,
-        margin: EdgeInsets.only(bottom: 6.0),
-      ),
-    );
-  }
-}
-
 class CartScreen extends StatefulWidget {
   @override
   _CartScreenState createState() => _CartScreenState();
 }
 
-class _CartScreenState extends State<CartScreen> with TickerProviderStateMixin {
-  //Variable used in the class
-  final _textColor = Color.fromRGBO(255, 103, 94, 1);
+class _CartScreenState extends State<CartScreen> {
+  final List<String> _itemList = <String>[];
   final _textController = TextEditingController();
-  final FocusNode _focusNode = FocusNode();
-  final List<ShoppingList> _listItems = [];
-  bool _isComposing = false;
-//Build method to create the basic visual layout of the app
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(255, 103, 94, 1),
       appBar: AppBar(
-        title: Center(
-          child: Text(
-            'Your Cart',
-            style: TextStyle(
-              color: _textColor,
-            ),
-          ),
-        ),
-        backgroundColor: Colors.white,
+        title: const Text('Your Cart'),
       ),
-      body: Column(
-        children: [
-          Flexible(
-            child: Center(
-              child: ListView.builder(
-                padding: EdgeInsets.all(8.0),
-                reverse: true,
-                itemBuilder: (_, int index) => _listItems[index],
-                itemCount: _listItems.length,
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 6.0),
-            child: _buildTextComposer(),
-          )
-        ],
-      ),
-    );
-  }
-  //build the textfield 
-  Widget _buildTextComposer() {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10.0),
-      child: Row(
-        children: [
-          Flexible(
-            child: TextField(
-              controller: _textController,
-              onChanged: (String text) {
-                setState(() {
-                  _isComposing = text.isNotEmpty;
-                });
-              },
-              onSubmitted: _isComposing ? _handleSubmitted : null,
-              decoration: InputDecoration(
-                hintText: 'Type an item',
-                fillColor: Colors.white,
-                filled: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-              ),
-              focusNode: _focusNode,
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 5.0),
-            child: Center(
-              child: IconButton(
-                icon: const Icon(
-                  Icons.add_outlined,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                onPressed: _isComposing ? () => _handleSubmitted(_textController.text) : null,
-              ),
-            ),
-          )
-        ],
+      body: ListView(children: _getItems()),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _displayDialog(context),
+        tooltip: 'Add Item',
+        child: Icon(Icons.add),
       ),
     );
   }
 
-  void _handleSubmitted(String text) {
+  void _addItem(String title) {
+    setState(() {
+      _itemList.add(title);
+    });
     _textController.clear();
-    setState(() {
-      _isComposing = false;
-    });
-    ShoppingList listItem = ShoppingList(
-      text: text,
-      animationController: AnimationController(
-        vsync: this,
-        duration: const Duration(milliseconds: 200),
-      ),
-    );
-    setState(() {
-      _listItems.insert(0, listItem);
-    });
-    _focusNode.requestFocus();
-    listItem.animationController.forward();
   }
 
-  @override
-  void dispose() {
-    for (var listItem in _listItems) {
-      listItem.animationController.dispose();
+  Widget _buildItem(String title) {
+    return ListTile(
+      title: Text(title),
+    );
+  }
+
+  _displayDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add your item to the list'),
+          content: TextField(
+            controller: _textController,
+            decoration: const InputDecoration(hintText: 'Enter item here'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('ADD'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _addItem(_textController.text);
+              },
+            ),
+            TextButton(
+              child: const Text('CANCEL'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+  List<Widget> _getItems() {
+    final List<Widget> _itemWidgets = <Widget>[];
+    for (String title in _itemList) {
+      _itemWidgets.add(_buildItem(title));
     }
-    super.dispose();
+    return _itemWidgets;
   }
 }
